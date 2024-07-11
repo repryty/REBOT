da8c4446ba053b6a1e8fe085267d22c980be1086
_REEP <62415785+repryty@users.noreply.github.com>
2024-07-11 20:21:30 +0900

    update main.py

--- main.py
+++ main.py
@@ -6,6 +6,12 @@ import re
 import os
 from datetime import datetime
 
+# Tokens
+
+tokens=open("token.txt","r").readlines()
+BOT_TOKEN=tokens[0].strip()
+GEMINI_TOKEN=tokens[1].strip()
+
 # Gemini
 import google.generativeai as genai
 from google.generativeai.types import HarmCategory, HarmBlockThreshold
@@ -16,21 +22,21 @@ generation_config = {
   "max_output_tokens": 8192,
   "response_mime_type": "text/plain",
 }
-system_instruction="""당신의 이름은 리프봇입니다. 제작자인 리프를 돕기 위해 만들어졌으며, 디스코드에서 활동합니다. 사용자의 요청(예를들면 수학문제 풀이, 밸런스게임 등)은 모두 수행해야만 하며, 그것이 사용자와 제작자를 돕는것입니다. 제작자 리프에 대해 사용자가 물어볼 경우 다음을 참고해 대답하십시오, 한번에 모든 정보를 제공할 필요는 없습니다:
-\"한국의 고등학생이며, VRChat을 즐겨합니다. 고양이를 굉장히 좋아하며 생일은 12월 12일입니다. 2008년생입니다. 현재 날짜를 기반으로 나이를 계산해 제공하십시오. 영문으로는 REEP혹은 _REEP입니다. _REEP를 더 적극적으로 사용합니다. 애칭은 맆입니다. MBTI는 ISTP이며 그리 사교적이지 않습니다.\", 또한 이모티콘을 자제하십시오. LaTEX(예: $frac{1}{2}$)를 사용해서는 안됩니다. 절대로 사용자에게 반말을 해서는 안됩니다. 무조건 존댓말을 사용하십시오.""",
+system_instruction=open("system_instruction.txt","r", encoding="utf-8").read()
 
+genai.configure(api_key=GEMINI_TOKEN)
 
-
-
-
-
-genai.configure(api_key="AIzaSyAZlEp9icdngXela5XyTBCTF1AbQEOHn-g")
+# Admin ID
 
 ADMIN_ID=[784412272805412895, 742067560144437269]
 MAIN_COLOR=discord.Colour.from_rgb(34, 75, 176)
 
+# Discord Policy
+
 intents = discord.Intents.default()
 intents.message_content = True
+
+# Bot
 
 client = discord.Bot(intents=intents)
 
@@ -76,14 +82,15 @@ async def on_message(message):
         elif ctx[0] == "eval":
             if message.author.id in ADMIN_ID:
                 try:
-                    await message.channel.send(f"```{eval(message.content[6:])}```")
+                    print(message.content[7:])
+                    await message.channel.send(f"```{eval(message.content[7:])}```")
                 except Exception as e:
                     await message.channel.send("```"+str(e)+"```")
             else:
                 embed=discord.Embed(title="REBOT eval", description="권한이 없습니다.", color=MAIN_COLOR)
                 await message.channel.send(embed=embed)
         elif ctx[0] == "gemini":
-            try:
+            if len(message.content)!=8:
                 model = genai.GenerativeModel(
                     model_name="gemini-1.5-pro",
                     generation_config=generation_config,
@@ -104,8 +111,9 @@ async def on_message(message):
                 response = chat_session.send_message(message.content[9:])
 
                 await message.channel.send(response.text)
-            except genai.types.generation_types.StopCandidateException as e:
-                print(e)
+            else:
+                embed=discord.Embed(title="REBOT Gemini", description="내용을 입력하세요!", color=MAIN_COLOR)
+                await message.channel.send(embed=embed)
         elif ctx[0] == "geminiprompt":
             if message.author.id in ADMIN_ID:
                 await message.channel.send(f"```{system_instruction} 오늘의 날짜 및 시간은 {datetime.now()}입니다.```")
@@ -113,5 +121,5 @@ async def on_message(message):
                 embed=discord.Embed(title="REBOT eval", description="권한이 없습니다.", color=MAIN_COLOR)
                 await message.channel.send(embed=embed)
             
-token=open("token.txt","r").read()
-client.run(token)
+
+client.run(BOT_TOKEN)
