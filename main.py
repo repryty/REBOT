61ee673098eadbbfb2de7383a699a5ccf317b8a6
_REEP <62415785+repryty@users.noreply.github.com>
2024-07-14 22:32:03 +0900

    update main.py

--- main.py
+++ main.py
@@ -22,7 +22,7 @@ GEMINI_TOKEN = os.getenv("REBOT_GEMINI_TOKEN")
 import google.generativeai as genai
 
 generation_config = {
-    "temperature": 0.8,
+    "temperature": 1.2,
     "top_p": 0.95,
     "top_k": 64,
     "max_output_tokens": 8192,
