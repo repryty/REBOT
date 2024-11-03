40b2cec370fb724f489864a4e8c8441dc9b396a3
repryty <main@repryty.dev>
2024-11-03 23:53:08 +0900

    commands.py 업데이트

--- src/commands.py
+++ src/commands.py
@@ -158,7 +158,7 @@ class Commands:
             return f"오류 발생: {e}"
 
     async def dice(self):
-        return f"🎲! {random.randint(0, self.args[0]}!"
+        return f"🎲! {random.randint(0, self.args[0])}!"
     
     async def gemini_reset(self) -> DiscordCommandResponse:
         await self.gemini.reset(id=self.message.guild.id)
